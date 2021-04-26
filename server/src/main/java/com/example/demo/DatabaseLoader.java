149f63d508b79a6f7e1a3c6c1d279b89edeae85c
aidenrourke <aidenrourke@gmail.com>
2021-04-26 03:11:40 -0400

    updates

--- server/src/main/java/com/example/demo/DatabaseLoader.java
+++ server/src/main/java/com/example/demo/DatabaseLoader.java
@@ -16,7 +16,7 @@ public class DatabaseLoader implements CommandLineRunner {
 
     @Override
     public void run(String... strings) throws Exception {
-        this.repository.save(new Employee("Frodo", "Baggins", "ring bearer"));
+        this.repository.save(new Employee("Bilbo", "Baggins", "burglar"));
     }
 
 }
